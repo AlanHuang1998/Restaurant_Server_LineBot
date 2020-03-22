207df9690d113f9c93751f48547f5133ba71714e
AlanHuang <36326341+AlanHuang1998@users.noreply.github.com>
2020-03-22 13:40:07 +0800

    0322 update
    0322 改版前的上傳

---
+++ App_Config.py
@@ -0,0 +1,11 @@
+serverId = 'Ue7d3f74f2e5108ceadf2faca070a3f50'
+
+LineBot_Token = '67euqRPDVhsbS1k5ghQpMuujjSd0XBh3Nv1AX0C/OOsMN2htGKGVTVaKbYLedMfw9oNZnr8I5m5twjXMUFWbouck95xkazWaPq9ub+tBI4QsUI8bAqb/vTT3+WoedCZkiVazBwE+fnJsZJSTOVR5OAdB04t89/1O/w1cDnyilFU='
+LineBot_Secret = '251f8fb208ce3cbe2ecad35fdec8c557'
+Client_LineBot_Token = 'dboqjKgzCLFQW8aAhN+ubZv0v78pdRXQ5fpSbXkPG/bzL9j15xW6RRJBibVuAtibU4amiYjYvTj3I4nxrNR+qrx4TARQ8zhkzpYJFq0r3EPt1cU0OBmO0F5gen1+uGXjqL6jrIfBl54GLe9ByH7zSQdB04t89/1O/w1cDnyilFU='
+
+Firebase_JsonPath = './firebase_resv1.json'
+
+# rich menu id
+# 控制端: 0e6b67a28840b8b78fd936da6a3e2e2a
+Richmenu_Server_Id = 'richmenu-0e6b67a28840b8b78fd936da6a3e2e2a'
